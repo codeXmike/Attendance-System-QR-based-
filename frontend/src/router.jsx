b54ed0d097ddf513166bac57340bf7e442e2bdfd
CodeXmike <simplymike07@gmail.com>
2025-09-23 19:37:44 +0100

    Created router.jsx

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,22 +1,35 @@
 // src/router.jsx
-import { createBrowserRouter } from "react-router-dom";
-import {useEffect} from "react";
-import { RouterProvider } from "react-router-dom";
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useEffect } from "react";
 
+import LecturerLogin from "../pages/lecturers/Login";
+import LecturerDashboard from "../pages/lecturers/Dashboard";
+import StudentDashboard from "../pages/students/Dashboard";
+import StudentLogin from "../pages/students/Login";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
-    
+    path: "/lecturer/login",
+    element: <LecturerLogin />,
+  },
+  {
+    path: "/lecturer/dashboard",
+    element: <LecturerDashboard />,
+  },
+  {
+    path: "/student/login",
+    element: <StudentLogin />,
+  },
+  {
+    path: "/student/dashboard",
+    element: <StudentDashboard />,
   },
 ]);
 
 export default function AppRouter() {
-    useEffect(() => {
-      document.title = "QR | Attendance System";
-    }, []);
-  
-    return <RouterProvider router={router} />;
-  }
+  useEffect(() => {
+    document.title = "QR | Attendance System";
+  }, []);
+
+  return <RouterProvider router={router} />;
+}
